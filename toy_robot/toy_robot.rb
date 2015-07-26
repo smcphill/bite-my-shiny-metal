ced0920127f135783e9e7ee86d842a561d4d20cb
Steven McPhillips <steven.mcphillips@gmail.com>
2015-07-26 14:42:52 +1000

    added toy_robot/toy_robot.rb

---
+++ toy_robot/toy_robot.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+require 'robot'
+require 'square_table'
+require 'instruction_parser'
+require 'simulation'
+
+module ToyRobot
+  COMMANDS = %i(place report move left right)
+  BEARINGS = %i(north south east west)
+
+  module_function
+
+  def simulate(input: [], output: $stdout)
+    simulation = Simulation.new
+    input.each do |line|
+      response = simulation.instruct line
+      output.puts response if response
+    end
+  end
+end
+
+ToyRobot.simulate(input: ARGF) if __FILE__ == $PROGRAM_NAME
