dda6d3c8097481383daf39798b4ad41b3b2d4710
Steven McPhillips <steven.mcphillips@gmail.com>
2015-07-26 14:42:52 +1000

    added toy_robot/instruction_parser.rb

---
+++ toy_robot/instruction_parser.rb
@@ -0,0 +1,58 @@
+module ToyRobot
+  class InstructionParser
+    def parse(instruction)
+      (command, arguments) = instruction.split ' '
+      request = {}
+      request[:command]   = symbolize command
+      request[:arguments] = parse_arguments arguments if arguments
+      request if valid_request?(request)
+    end
+
+    private
+
+    def valid_request?(request)
+      valid_command = COMMANDS.include?(request[:command])
+      if request[:command] == :place
+        valid_arguments = valid_arguments?(request[:arguments])
+      else
+        valid_arguments = !request.key?(:arguments)
+      end
+
+      valid_command && valid_arguments
+    end
+
+    def valid_arguments?(arguments)
+      (
+        BEARINGS.include?(arguments[:bearing]) &&
+        valid_coordinates?(arguments[:coordinates])
+      )
+    end
+
+    def valid_coordinates?(coordinates)
+      (
+        coordinates[:x].is_a?(Integer) &&
+        coordinates[:y].is_a?(Integer)
+      )
+    end
+
+    def parse_arguments(arguments)
+      (x, y, bearing) = arguments.split ','
+      arguments = {}
+      arguments[:bearing]         = symbolize bearing
+      arguments[:coordinates]     = {}
+      arguments[:coordinates][:x] = parse_numeric x
+      arguments[:coordinates][:y] = parse_numeric y
+      arguments
+    end
+
+    def parse_numeric(input)
+      Integer input
+    rescue
+      nil
+    end
+
+    def symbolize(input)
+      "#{input}".downcase.to_sym
+    end
+  end
+end
