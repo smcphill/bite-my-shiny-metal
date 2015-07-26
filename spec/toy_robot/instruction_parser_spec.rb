085027ca87de979f32bafdf46d30a3d424e1c8c4
Steven McPhillips <steven.mcphillips@gmail.com>
2015-07-26 14:42:52 +1000

    added spec/toy_robot/instruction_parser_spec.rb

---
+++ spec/toy_robot/instruction_parser_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe ToyRobot::InstructionParser do
+  describe '#parse' do
+    it 'parses the instruction command' do
+    end
+
+    context 'when the command is :place' do
+    end
+
+    context 'when the command is known but not place' do
+    end
+
+    context 'when the command is not known' do
+    end
+  end
+end
