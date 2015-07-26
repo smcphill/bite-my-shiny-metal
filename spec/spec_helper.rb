4b396ab02c6129665f774401af9d00ed23c1b6f0
Steven McPhillips <steven.mcphillips@gmail.com>
2015-07-27 00:37:04 +1000

    spec: validate mocks; run coverage only when asked

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,16 @@
 require 'simplecov'
+
+RSpec.configure do |config|
+  config.mock_with :rspec do |mocks|
+    mocks.verify_doubled_constant_names = true
+  end
+end
+
 SimpleCov.start do
   coverage_dir 'tmp/coverage'
   add_filter ['/.bundle/', '/.git/', '/spec/', '/bin/', '/config/']
-end
-
-SimpleCov.minimum_coverage 100
+  SimpleCov.minimum_coverage 100
+end if ENV['COVERAGE']
 
 $LOAD_PATH.unshift File.expand_path('../../toy_robot', __FILE__)
 require 'toy_robot'
